b3144c450042db5a8e87627ef3b1cfab3cecc003
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-05 23:17:36 +0200

    Update C++_abs_val_template.cpp

--- C++_abs_val_template/C++_abs_val_template.cpp
+++ C++_abs_val_template/C++_abs_val_template.cpp
@@ -1,7 +1,7 @@
 
 
 			/*
-			A program which prompts the user to enter
+			A program that prompts the user to enter
 			an integer which is then passed as a reference
 			to the 'abs' template function. The input is validated
 			and the integers` absolute value is printed on the screen.
@@ -11,41 +11,41 @@
 
 
 
-#include <iostream>																	// including the iostream directive
-#include <stdlib.h>																	// including the stdlinb header file
+#include <iostream>														// including the iostream directive
+#include <stdlib.h>														// including the stdlinb header file
 
-using namespace std;																// using the std namespace
-
-
+using namespace std;														// using the std namespace
 
 
-template <class T>																	// declaring template 'abs'
-T abs(T num)																		// which accepts 1 parameter
+
+
+template <class T>														// declaring template 'abs'
+T abs(T num)															// which accepts 1 parameter
 {
-	return abs(num);																// returning the absolute value of num
+	return abs(num);													// returning the absolute value of num
 }
 
 int main()																			
 {
 
-	int x = 1;																		// an integer to hold the users input
+	int x = 1;														// an integer to hold the users input
 
 
-	while (x != 0)																	// entering the loop
+	while (x != 0)														// entering the loop
 	{
-		cout << "\n Enter an integher (type 0 to quit)> ";							// prompting the user to enter an integer
+		cout << "\n Enter an integher (type 0 to quit)> ";								// prompting the user to enter an integer
 		cin >> x;
 
-		if (x == 0)																	// validating the input
+		if (x == 0)													// validating the input
 			continue;
 
-		cout << "\nThe absolute value of " << x << " is '" << abs(x) << "'\n";		// calling the abs function and printing the resul on the screen
-	}
+		cout << "\nThe absolute value of " << x << " is '" << abs(x) << "'\n";						// calling the abs function and printing 
+	}															// the result on the screen
 
 	cout << '\n';
 
 
 
 
-	return 0;																		// exiting the program
+	return 0;														// exiting the program
 }
