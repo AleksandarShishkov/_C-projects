bfc79d1f7e83bfb4336825a2cf04c7c17860cb5e
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-06 01:49:27 +0200

    Update C++_total_template.cpp

--- C++_total_template/C++_total_template.cpp
+++ C++_total_template/C++_total_template.cpp
@@ -18,24 +18,24 @@
 
 
 
-#include <iostream>															// including the iostream directive
+#include <iostream>														// including the iostream directive
 
 using namespace std;														// using the std namespace
 
 
 
 
-template <class T>															// declaring template functiont total
+template <class T>														// declaring template functiont total
 T total(T num)
 {
-	static T total = 0;														// static total to keep the running total
+	static T total = 0;													// static total to keep the running total
 	
-	if (num == -1)															// validating the input
+	if (num == -1)														// validating the input
 	{
 		return total;
 	}
 	else
-		total += num;														// adding the value entered to the total variable
+		total += num;													// adding the value entered to the total variable
 
 }
 
@@ -44,29 +44,29 @@ T total(T num)
 int main()
 {
 
-	int numI = 0;															// an int to hold the integers
-	double numD = 0;														// a double to hold the doubles
+	int numI = 0;														// an int to hold the integers
+	double numD = 0;													// a double to hold the doubles
 	
-	int choice;																// an integer to hold the users choice
+	int choice;														// an integer to hold the users choice
 
-	cout << "\n\tChoose between the following: \n\n" <<						// a menu for the options
+	cout << "\n\tChoose between the following: \n\n" <<									// a menu for the options
 		"\t'1' for integer\n" <<
 		"\t'2' for double\n" <<
 		"\t'-1' to stop entering\n";
 	cout << "> ";
-	cin >> choice;															// prompting the user to enter the option selected
+	cin >> choice;														// prompting the user to enter the option selected
 
-	switch (choice)															// using switch statement for the choice
+	switch (choice)														// using switch statement for the choice
 	{
 	case 1:
 
-		while (numI != -1)													// entering the while loop
+		while (numI != -1)												// entering the while loop
 		{
-			cout << "\nEnter an integer> ";									// prompting the user to enter a value
+			cout << "\nEnter an integer> ";										// prompting the user to enter a value
 			cin >> numI;
 
-			if (numI != -1)													// validating the input
-				total(numI);												// passing the value to the 'total' template
+			if (numI != -1)												// validating the input
+				total(numI);											// passing the value to the 'total' template
 			else
 				cout << "\n\tThe total ot the entered integers is: " <<		// printing the result
 				total(numI) << '\n';								
@@ -76,13 +76,13 @@ int main()
 
 	case 2:
 
-		while (numD != -1)													// entering the while loop
+		while (numD != -1)												// entering the while loop
 		{
-			cout << "\nEnter a double> ";									// prompting the user to enter a value
+			cout << "\nEnter a double> ";										// prompting the user to enter a value
 			cin >> numD;
 
-			if (numD != -1)													// validating the input
-				total(numD);												// passing the value to the 'total' template
+			if (numD != -1)												// validating the input
+				total(numD);											// passing the value to the 'total' template
 			else
 				cout << "\n\tThe total of the entered doubles is: " <<		// printing the result
 				total(numD) << "\n";
@@ -90,7 +90,7 @@ int main()
 
 		break;
 
-	default:																// validating the choice selected
+	default:														// validating the choice selected
 
 		cout << "\nInvalid input. Try again!\n";
 		break;
@@ -101,5 +101,5 @@ int main()
 
 
 
-	return 0;																// exiting the program
+	return 0;														// exiting the program
 }
