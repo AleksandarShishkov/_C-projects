1170c2710ae7142eb695b36c79206f3558253968
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-08 23:05:34 +0200

    Update BasicShape.h

--- C++_pure_virtual_class/BasicShape.h
+++ C++_pure_virtual_class/BasicShape.h
@@ -1,30 +1,31 @@
 
 
+
 #ifndef BASICSHAPE_H
 #define BASICSHAPE_H
 
-#include <iostream>															// including the iostream library
-#include <cmath>															// includeing the cmath library
+#include <iostream>										// including the iostream library
+#include <cmath>										// includeing the cmath library
 
-using namespace std;														// using the std namespace
+using namespace std;										// using the std namespace
 
 
 
-class BasicShape															// defining pure virtual class BasicShape
+class BasicShape										// defining pure virtual class BasicShape
 {
 
 protected:
 
-	double area;															// a protected double to hold the area
+	double area;										// a protected double to hold the area
 
 
 public:
 
 
-	double get_area();														// getter for the area
-	virtual double calc_area() = 0;											// pure virtual function calc_area
+	double get_area();									// getter for the area
+	virtual double calc_area() = 0;								// pure virtual function calc_area
 	
-	virtual ~BasicShape()													// virtual destructor
+	virtual ~BasicShape()									// virtual destructor
 	{	}
 };
 
