bd161bf92cc7a6f4393023fe2b17ab7cc6e6f19a
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-11 23:59:56 +0200

    Add files via upload

---
+++ C++_class_and_exceptions/Rectangle.cpp
@@ -0,0 +1,47 @@
+
+
+
+
+#include "Rectangle.h"													// including the Rectangle`s class header											
+
+
+Rectangle::Rectangle() :lenght(1), width(1)								// assigning 1 to lenght and width
+{
+
+}
+
+void Rectangle::set_lenght()
+{
+	cout << "\nEnter the lenght of the rectangle (1.0 - 20.0)> ";		// prompting the user to enter the lenght
+	cin >> lenght;
+
+	if (lenght < 1 || lenght > 20)										// validating the input
+		throw Exception_lenght(lenght);									// throwing an exception if invalid
+}
+
+void Rectangle::set_width()
+{
+	cout << "\nEnter the width of the rectangle (1.0 - 20.0)> ";		// prompting the user to enter the width
+	cin >> width;
+
+	if (width < 1 || width>20)											// validating the input
+		throw Exception_width(width);									// throwing exception if invalid
+}
+
+double Rectangle::get_area()
+{
+	double area = lenght * width;										// calculating the area
+	return area;														// returning the value
+}
+
+double Rectangle::get_perimeter()
+{
+	double perimeter = 2 * (lenght + width);							// calculating the perimeter
+	return perimeter;													// returning the value
+}
+
+Rectangle::~Rectangle()
+{
+
+}
+
---
+++ C++_class_and_exceptions/Rectangle.h
@@ -0,0 +1,82 @@
+
+
+
+
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <iostream>														// including iostream directory
+
+using namespace std;													// using the std namespace
+
+
+
+class Rectangle
+{
+
+private:
+
+	double lenght;														// a private double to hold the lenght
+	double width;														// a private double to hold the width
+
+public:
+
+	Rectangle();														// default constructor
+
+	
+	class Exception_lenght												// exception class for the lenght
+	{
+	private:
+
+		double l;														// a private double to hold the lenght
+
+	public:
+
+		Exception_lenght(double lenght)									// constructor with one paramter for
+		{																// the lenght
+
+			l = lenght;													// assigning the lenght to l
+		}
+
+		double get_l()													// getter for l
+		{
+			return l;													// returning l
+		}	
+	};
+
+	class Exception_width												// exception class for the width
+	{
+	private:
+
+		double w;														// a private double to hold the width									
+
+	public:
+
+		Exception_width(double width)									// constructor with one paramter for	
+		{																// the lenght
+
+			w = width;													// assigning the width to w
+		}
+
+		double get_w()													// getter for w
+		{
+			return w;													// returning w
+		}
+	};
+	
+	void set_lenght();													// setter for the lenght
+	void set_width();													// setter for the width
+
+
+
+	double get_area();													// getter for the area
+	double get_perimeter();												// getter for the parameter
+		
+	~Rectangle();														// destructor
+
+};
+
+
+#endif
+
+
