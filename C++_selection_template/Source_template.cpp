d6950fc5a472a930fc12a484df82e9905c6e5660
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-12 16:57:11 +0200

    Update Source_template.cpp

--- C++_selection_template/Source_template.cpp
+++ C++_selection_template/Source_template.cpp
@@ -15,15 +15,15 @@
 
 #include <iostream>											// including the iostream directory
 
-using namespace std;										// using the std namescpace
+using namespace std;											// using the std namescpace
 
 
-template<class D, class D1>									// declating template function
-D get_data(D* arr, D1 size)									// get_data() with two arguments,
-{															// one of which is a pointer to an array
+template<class D, class D1>										// declating template function
+D get_data(D* arr, D1 size)										// get_data() with two arguments,
+{													// one of which is a pointer to an array
 
 	
-	cout << "\nEnter " << size <<							// prompting the user to enter the array
+	cout << "\nEnter " << size <<									// prompting the user to enter the array
 		" elements.\n";										// elements
 
 	
@@ -31,41 +31,41 @@ D get_data(D* arr, D1 size)									// get_data() with two arguments,
 	{
 		cout << "Element " << i + 1 << "> ";
 		cin >> *(arr + i);									// assigning the inputted value to
-	}														// the current element
+	}												// the current element
 
 
 	return *arr;											// returning a pointer to the array
 
 }
 
-template<class S, class S1>									// declaring template function
-S sort(S *arr, S1 size)										// sort() with two arguments,
-{															// one of which is a pointer to an array
+template<class S, class S1>										// declaring template function
+S sort(S *arr, S1 size)											// sort() with two arguments,
+{													// one of which is a pointer to an array
 
 
 	for (int i = 0; i < size - 1; i++)
 	{
 		S min = *(arr + i);									// assigning the first element to
-															// the S min variable
+													// the S min variable
 
 		S1 min_index = i;									// assigning the current index to
-															// the S min_index variable
+													// the S min_index variable
 
 		for (int j = i + 1; j < size; j++)
 		{
-			if (min > * (arr + j))							// validating the next element
+			if (min > * (arr + j))								// validating the next element
 			{
 				min = *(arr + j);							// assigning it to min if with
-															// smaller value
+													// smaller value
 
 				min_index = j;								// assigning the current index to 
-															// min_index if the tested exprecion is
-															// valid
+													// min_index if the tested exprecion is
+													// valid
 			}
 		
 		}
 
-			S temp = *(arr + i);							// swapping the elements
+			S temp = *(arr + i);								// swapping the elements
 			*(arr + i) = min;
 			*(arr + min_index) = temp;
 		
@@ -74,13 +74,13 @@ S sort(S *arr, S1 size)										// sort() with two arguments,
 	return *arr;											// returning a pointer to an array
 }
 
-template<class P, class P1>									// declaring a remplate function
-void print(P* arr, P1 size)									// print() with two arguments,
-{															// one of which is a pointer to the array
+template<class P, class P1>										// declaring a remplate function
+void print(P* arr, P1 size)										// print() with two arguments,
+{													// one of which is a pointer to the array
 
 	for (int i = 0; i < size; i++)
 	{
-		cout << *(arr + i) << " ";							// printing the elements on the screen
+		cout << *(arr + i) << " ";								// printing the elements on the screen
 	}
 }
 
@@ -88,15 +88,15 @@ void print(P* arr, P1 size)									// print() with two arguments,
 int main()
 {
 	
-	int size;												// an integer to hold the size
+	int size;											// an integer to hold the size
 	int choice = 0;											// an integer to hold the choice
 	
 	while (choice != 4)										// entering the while loop
 	{
 
 
-		cout << "\n\n\tSelect between the following: \n";	// printing a menu
-		cout << "\n'1' for an array of integers";			// of 4 options
+		cout << "\n\n\tSelect between the following: \n";					// printing a menu
+		cout << "\n'1' for an array of integers";						// of 4 options
 		cout << "\n'2' for an array of doubles";
 		cout << "\n'3' for an array of characters";
 		cout << "\n'4' to quit";
@@ -108,7 +108,7 @@ int main()
 			cout << "\n\tYou`ve selected to " <<
 				"sort an aray of integers\n";
 
-			cout << "\nEnter the size> ";					// prompting the user to enter
+			cout << "\nEnter the size> ";							// prompting the user to enter
 			cin >> size;									// the size
 
 			while (size < 1)								// validating the input
@@ -117,28 +117,28 @@ int main()
 				cin >> size;
 			}
 
-			int* arrInt = new int[size];					// allocating memory for the array
-															// of integers
+			int* arrInt = new int[size];							// allocating memory for the array
+													// of integers
 
 
-			get_data(arrInt, size);							// calling get_data() function
+			get_data(arrInt, size);								// calling get_data() function
 			cout << "\nThe input array is: \n";
-			print(arrInt, size);							// calling prin() function
+			print(arrInt, size);								// calling prin() function
 
 			sort(arrInt, size);								// calling sort() function
 			cout << "\n\nThe sorted array is: \n";
-			print(arrInt, size);							// calling print() function
+			print(arrInt, size);								// calling print() function
 
-			delete arrInt;
+			delete arrInt;									// relesing the allocated memory
 
 		}
-		else if (choice == 2)								// validating the input
+		else if (choice == 2)									// validating the input
 		{
 
 			cout << "\n\tYou`ve selected to " <<
 				"sort and array of doubles\n";
 
-			cout << "\nEnter the size> ";					// prompting the user to enter
+			cout << "\nEnter the size> ";							// prompting the user to enter
 			cin >> size;									// the size
 
 			while (size < 1)								// validating the input
@@ -147,26 +147,26 @@ int main()
 				cin >> size;
 			}
 
-			double* arrDouble = new double[size];			// allocating memory for the array
-															// of doubles
+			double* arrDouble = new double[size];						// allocating memory for the array
+													// of doubles
 
-			get_data(arrDouble, size);						// calling get_data() function
+			get_data(arrDouble, size);							// calling get_data() function
 			cout << "\nThe input array is: \n";
-			print(arrDouble, size);							// calling print() function
+			print(arrDouble, size);								// calling print() function
 
-			sort(arrDouble, size);							// calling sort() function
+			sort(arrDouble, size);								// calling sort() function
 			cout << "\n\nThe sorted array is: \n";
-			print(arrDouble, size);							// calling print() function
+			print(arrDouble, size);								// calling print() function
 
-			delete arrDouble;
+			delete arrDouble;								// relesing the allocated memory
 
 		}
-		else if (choice == 3)								// validating the input
+		else if (choice == 3)									// validating the input
 		{
 			cout << "\n\tYou`ve selected to " <<
 				"sort an array of chars\n";
 
-			cout << "\nEnter the size: ";					// prompting the user to enter
+			cout << "\nEnter the size: ";							// prompting the user to enter
 			cin >> size;									// the size
 
 			while (size < 1)								// validating the input
@@ -175,33 +175,33 @@ int main()
 				cin >> size;
 			}
 
-			char* arrChar = new char[size];					// allocating memory for the array
-															// of chars
+			char* arrChar = new char[size];							// allocating memory for the array
+													// of chars
 
-			get_data(arrChar, size);						// calling get_data() function
+			get_data(arrChar, size);							// calling get_data() function
 			cout << "\nThe input array is: \n";
-			print(arrChar, size);							// calling print() function
+			print(arrChar, size);								// calling print() function
 
-			sort(arrChar, size);							// calling sort() function
+			sort(arrChar, size);								// calling sort() function
 			cout << "\n\nThe sorted array is: \n";
-			print(arrChar, size);							// calling print() function
+			print(arrChar, size);								// calling print() function
 
-			delete arrChar;
+			delete arrChar;									// relesing the allocated memory
 
 		}
-		else if (choice == 4)								// validating the input
+		else if (choice == 4)									// validating the input
 		{
-			cout << "\nYou`ve selected option " <<			// prinitng a messge indicating
-				"'4'\nThe program has ended\n";				// that the program has ended
+			cout << "\nYou`ve selected option " <<						// prinitng a messge indicating
+				"'4'\nThe program has ended\n";						// that the program has ended
 		}
-		else												// validating the input
+		else											// validating the input
 			cout << "\n\nInvalid input. Try again>\n";
 
-	}														// exiting the while loop
+	}												// exiting the while loop
 
 
 
-	return 0;												// terminating the program
+	return 0;											// terminating the program
 }
 
 
