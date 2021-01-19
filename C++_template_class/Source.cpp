65f6b68954bd980da959ac1c1f86c6e9e0a05570
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-19 19:14:58 +0200

    Update Source.cpp

--- C++_template_class/Source.cpp
+++ C++_template_class/Source.cpp
@@ -5,8 +5,7 @@
 		/*
 		 A C++ program that uses a template class
 		 to calculate the largest, the smallest and
-		 the middle value entered by the user and to 
-		 display da output.
+		 the middle value entered by the user.
 		 */
 
 
@@ -14,7 +13,7 @@
 
 
 
-#include "TemplateClass.h"																	// including TemplateClass header file
+#include "TemplateClass.h"												// including TemplateClass header file
 
 
 int main()
@@ -24,9 +23,9 @@ int main()
 
 	TemplateClass<double>* objDouble = new TemplateClass<double>;							// a pointer to a double TemplateClass`s object without parameters
 
-	TemplateClass<float> objF(22.3, 33.4, 44.5);											// a float TemplateClass`s object with parameters
+	TemplateClass<float> objF(22.3, 33.4, 44.5);									// a float TemplateClass`s object with parameters
 
-	TemplateClass<float>* objFloat = &objF;													// a pointer to the float TemplateClass`s object
+	TemplateClass<float>* objFloat = &objF;										// a pointer to the float TemplateClass`s object
 
 
 
@@ -36,9 +35,9 @@ int main()
 
 	cout << "\n\nEnter the three doubles:";
 
-	objDouble->set_num1();																	// setter for the first double number
-	objDouble->set_num2();																	// setter for the second double number
-	objDouble->set_num3();																	// setter for the third double number
+	objDouble->set_num1();												// setter for the first double number
+	objDouble->set_num2();												// setter for the second double number
+	objDouble->set_num3();												// setter for the third double number
 
 	cout << "\nThe largest double is: " << objDouble->get_largest();						// printing the largest double
 	cout << "\nThe smallest double is: " << objDouble->get_smallest();						// printing the smallest double
@@ -49,7 +48,7 @@ int main()
 	cout << "\nThe smallest float is: " << objFloat->get_smallest();						// printing the smallest float
 
 
-	cout << "\n\n\tThe program has ended!\n";												// a message indicating the end of the program
+	cout << "\n\n\tThe program has ended!\n";									// a message indicating the end of the program
 	
 
 
