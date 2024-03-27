c917ad9d87162b82d883c64c0eaa357970e15f73
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:19:36 +0200

    Update Source.cpp

--- C++_polimorphism/Source.cpp
+++ C++_polimorphism/Source.cpp
@@ -1,46 +1,45 @@
-#include "PackagedFood.h"											// including the PackagedFood header file
-#include "FreshFood.h"												// including the FreshFood header file
+#include "PackagedFood.h"											
+#include "FreshFood.h"												
 
 
 
 
-int main()													// main function
+int main()													
 {
 
-	PackagedFood p_food1(15.5, 32315, "Potatoes");								// instance of the PackageFood class
-	PackagedFood p_food2;											// instance of the PackageFood class
-	PackagedFood* p_ptrFood = &p_food2;									// a pointer to the PackageFood class`s instance
+	PackagedFood p_food1(15.5, 32315, "Potatoes");								
+	PackagedFood p_food2;											
+	PackagedFood* p_ptrFood = &p_food2;									
 
-	FreshFood f_food1(0.50, 14.5, 12341, "Oranges");							// instance of the FreshFood class
-	FreshFood f_food2;											// instance of the FreshFood class
-	FreshFood* f_ptrFood = &f_food1;									// a pointer to the FreshFood class`s instance
-
-
-	p_food1.set_numUnits();											// testing the setter for the number of units
-	p_food1.printer();											// testing the printer function	
-
-	p_food2.scanner();											// testing the scanner function
-	p_food2.get_numUnits();											// testing the setter for the number of units
-	p_food2.printer();											// testing the printer function
-
-	p_ptrFood->setName();											// testing the setter for the name
-	p_ptrFood->setCode();											// testing the setter for the code
-	p_ptrFood->set_numUnits();										// testing the setter for the number of units
-	p_ptrFood->set_pricePerUnit();										// testing the setter for the price
-	p_ptrFood->printer();											// printing the result
+	FreshFood f_food1(0.50, 14.5, 12341, "Oranges");							
+	FreshFood f_food2;											
+	FreshFood* f_ptrFood = &f_food1;									
 
 
-	f_food1.printer();											// testing the printer function
+	p_food1.set_numUnits();											
+	p_food1.printer();											
 
-	f_ptrFood->scanner();											// testing the scanner function
-	f_ptrFood->printer();											// printing the result
+	p_food2.scanner();											
+	p_food2.get_numUnits();											
+	p_food2.printer();											
+	p_ptrFood->setName();											
+	p_ptrFood->setCode();											
+	p_ptrFood->set_numUnits();										
+	p_ptrFood->set_pricePerUnit();										
+	p_ptrFood->printer();											
 
-	f_food2.setName();											// testing the setter for the name
-	f_food2.set_pricePerKilo();										// testing the setter for the price per kilo
-	f_food2.set_weight();											// testing the setter for the weight
-	f_food2.printer();											// printing the result
 
-	cout << f_food1.getName() << '\n';									// testing the getter for the name
+	f_food1.printer();											
+
+	f_ptrFood->scanner();											
+	f_ptrFood->printer();											
+
+	f_food2.setName();											
+	f_food2.set_pricePerKilo();										
+	f_food2.set_weight();											
+	f_food2.printer();											
+
+	cout << f_food1.getName() << '\n';									
 	cout << f_food1.getTotal() << '\n'; // testing the getter for the total amount
 
 
