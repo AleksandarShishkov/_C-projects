974d8d83f7a37c1b82d12efc657401e880b4ce7b
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:24:02 +0200

    Update Rectangle.cpp

--- C++_pure_virtual_class/Rectangle.cpp
+++ C++_pure_virtual_class/Rectangle.cpp
@@ -5,60 +5,60 @@
 
 
 
-Rectangle::Rectangle() :width(0), lenght(0)							// assigning default values to
-{												// the width and the lenght
+Rectangle::Rectangle() :width(0), lenght(0)							
+{												
 
 }
 
 Rectangle::Rectangle(long _width, long _lenght)
 {
-	if (_width <= 0)									// validating _width
-		throw Exception_width();							// throwing an exception if invalid
-	else if (_lenght <= 0)									// validating _lenght
-		throw Exception_lenght();							// throwing an exception if invalid
+	if (_width <= 0)									
+		throw Exception_width();							
+	else if (_lenght <= 0)									
+		throw Exception_lenght();							
 	else
 	{
-		width = _width;									// assigning _width to width
-		lenght = _lenght;								// assigning _lenght to lenght
+		width = _width;									
+		lenght = _lenght;								
 
-		calc_area();									// calling the calc_area function
+		calc_area();									
 	}	
 
 }
 
 void Rectangle::set_width()													
 {
-	cout << "\nEnter the rectangle`s width> ";						// prompting the user to enter the width
+	cout << "\nEnter the rectangle`s width> ";						
 	cin >> width;
 
-	if (width < 0)										// validating the input
-		throw Exception_width();							// throwing an exception if invalid
+	if (width < 0)										
+		throw Exception_width();							
 }
 void Rectangle::set_lenght()
 {
-	cout << "\nEnter the rectangle`s lenght> ";						// prompting the user to enter the lenght
+	cout << "\nEnter the rectangle`s lenght> ";						
 	cin >> lenght;
 
-	if (lenght < 0)										// validating the input
-		throw Exception_lenght();							// throwing an exception if invalid
+	if (lenght < 0)										
+		throw Exception_lenght();							
 }
 
 long Rectangle::get_width()
 {
-	return width;										// returning the width
+	return width;										
 }
 long Rectangle::get_lenght()
 {
-	return lenght;										// returning the lenght
+	return lenght;										
 }	
 
 double Rectangle::calc_area()
 {
-	area = double(width) * double(lenght);							// calculating the area
-	return area;										// returning the area
+	area = double(width) * double(lenght);							
+	return area;										
 }
 
-Rectangle::~Rectangle()										// declaring the destructor
+Rectangle::~Rectangle()										
 {
 
 }
