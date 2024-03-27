475060d2062daa899f5662a3513fb03a5fb83f01
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:15:22 +0200

    Update FreshFood.h

--- C++_polimorphism/FreshFood.h
+++ C++_polimorphism/FreshFood.h
@@ -1,35 +1,34 @@
 #ifndef FRESHFOOD_H
 #define FRESHFOOD_H
-#include"Products.h"												// including the Products header file
+#include"Products.h"												
 
-
-class FreshFood :public Products										// defining derived class FreshFood
+class FreshFood :public Products										
 {
 
 private:
 
-	double weight;												// a private double to hold the weight
-	double pricePerKilo;											// a private double to hold the price per kilo
+	double weight;												
+	double pricePerKilo;											
 
 public:
 
-	FreshFood();												// default constructor
-	FreshFood(double, double, long int, string);								// constructor with parameters for the weight, the price per kilo,
-														// the barcode and the name
+	FreshFood();												
+	FreshFood(double, double, long int, string);								
+														
 
-	void set_weight();											// setter for the weight
-	double get_weight();											// getter for the weight
+	void set_weight();											
+	double get_weight();											
 
-	void set_pricePerKilo();										// setter for the price per kilo
-	double get_pricePerKilo();										// getter for the price per kilo
+	void set_pricePerKilo();										
+	double get_pricePerKilo();										
 
-	double getTotal();											// getter for the total price
+	double getTotal();											
 
-	void setCode() override;										// overriding the virtual setter for the products barcode
-	void setName() override;										// overiding the virtual setter for the products name
+	void setCode() override;										
+	void setName() override;										
 
-	void scanner();												// overiding the pure virtual scanner
-	void printer() override;										// overriding the virtual printer
+	void scanner();												
+	void printer() override;										
 
 
 
