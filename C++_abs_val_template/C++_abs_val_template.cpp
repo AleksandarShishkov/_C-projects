1eba472f894d16605ae1e0422e15c7d269449efb
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 21:43:26 +0200

    Update C++_abs_val_template.cpp

--- C++_abs_val_template/C++_abs_val_template.cpp
+++ C++_abs_val_template/C++_abs_val_template.cpp
@@ -1,51 +1,42 @@
 
 
-			/*
-			A program that prompts the user to enter
-			an integer which is then passed as a reference
-			to the 'abs' template function. The input is validated
-			and the integers` absolute value is printed on the screen.
-			*/
+
+
+#include <iostream>														
+#include <stdlib.h>														
+
+using namespace std;														
 
 
 
 
-
-#include <iostream>														// including the iostream directive
-#include <stdlib.h>														// including the stdlinb header file
-
-using namespace std;														// using the std namespace
-
-
-
-
-template <class T>														// declaring template 'abs'
-T abs(T num)															// which accepts 1 parameter
+template <class T>														
+T abs(T num)															
 {
-	return abs(num);													// returning the absolute value of num
+	return abs(num);													
 }
 
 int main()																			
 {
 
-	int x = 1;														// an integer to hold the users input
+	int x = 1;														
 
 
-	while (x != 0)														// entering the loop
+	while (x != 0)														
 	{
-		cout << "\n Enter an integher (type 0 to quit)> ";								// prompting the user to enter an integer
+		cout << "\n Enter an integher (type 0 to quit)> ";								
 		cin >> x;
 
-		if (x == 0)													// validating the input
+		if (x == 0)													
 			continue;
 
-		cout << "\nThe absolute value of " << x << " is '" << abs(x) << "'\n";						// calling the abs function and printing 
-	}															// the result on the screen
+		cout << "\nThe absolute value of " << x << " is '" << abs(x) << "'\n";						 
+	}															
 
 	cout << '\n';
 
 
 
 
-	return 0;														// exiting the program
+	return 0;														
 }
