28aa6c2d1d7a3df9589e97687d6963e418422ff3
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:02:37 +0200

    Update TwoDayPackage.h

--- C++_inheritance_1/TwoDayPackage.h
+++ C++_inheritance_1/TwoDayPackage.h
@@ -3,34 +3,33 @@
 #include "Package.h"
 
 
-class TwoDayPackage :public Package										// derived class TwoDatPackage
+class TwoDayPackage :public Package										
 {
 
 private:
 
-	double flat_fee = 0.0;											// a double to hold the additional delivery fee
+	double flat_fee = 0.0;											
 
 
 public:
 
-	TwoDayPackage();											// default constructor
+	TwoDayPackage();											
 
-	TwoDayPackage(double, string, string, string, string);							// overloaded constructor with an argument for the flat_fee 
-														// that passes values to the base class constructor
-
-
+	TwoDayPackage(double, string, string, string, string);							 
+														
 
 
-	void set_flat_fee();											// setter for the fee
+
+	void set_flat_fee();											
 
 
-	virtual double calculateCost() override									// overriding the base calculateCost() function
+	virtual double calculateCost() override									
 	{
 		double total = (flat_fee + cost_per_kg) * weight_package;
 		return total;
 	}
 
-	void print();												// print() function to print the result
+	void print();												
 
 
 };
