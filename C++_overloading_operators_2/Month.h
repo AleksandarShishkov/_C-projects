b89bc0e4f583ca10b1d310413cbfd808d1120942
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:11:46 +0200

    Update Month.h

--- C++_overloading_operators_2/Month.h
+++ C++_overloading_operators_2/Month.h
@@ -2,49 +2,49 @@
 #define MONTH_H
 
 
-#include <iostream>										// including the input output libraty
-#include <string>										// including the string library
+#include <iostream>										
+#include <string>										
 #include <fstream>
 
 
 
-using namespace std;										// using the std namespace
+using namespace std;										
 
-class Month											// defining class Month
+class Month											
 {
 
 private:
 
-	string name;										// a string to hold the name of the month
-	int monthNum;										// an integer to hold the number of the month
+	string name;										
+	int monthNum;										
 
 
 
 public:
 
-	Month();										// default constructor
-	Month(string);										// overloaded constructor with parameter for the name
-	Month(int);										// overloaded constructor with parameter for the number
+	Month();										
+	Month(string);										
+	Month(int);										
 
-	void set_name();									// setter for the name
-	string get_name();									// getter for the name
+	void set_name();									
+	string get_name();									
 
-	void set_monthNum();									// setter for the number
-	int get_monthNum();									// getter for the number
+	void set_monthNum();									
+	int get_monthNum();									
 
-	Month operator++();									// overloaded prefix '++' operator
-	Month operator++(int);									// overloaded postfix '++' operator
-	Month operator--();									// overloaded prefix '--' operator
-	Month operator--(int);									// overloaded postirx '--' operator
+	Month operator++();									
+	Month operator++(int);									
+	Month operator--();									
+	Month operator--(int);									
 
-	void print();										// print() function to print the result
-
-
-	friend ostream& operator<<(ostream&, Month&);						// friend overloaded ostream '<<' fucntion/ prototype
-	friend istream& operator>>(istream&, Month&);						// friend overloaded istream '>>' function/ prototype
+	void print();										
 
 
-	~Month();										// destructor
+	friend ostream& operator<<(ostream&, Month&);						
+	friend istream& operator>>(istream&, Month&);						
+
+
+	~Month();										
 
 
 };
