ffbc698493e116e3266526fa1eb3a393ccd27753
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:35:37 +0200

    Update C++_templates.cpp

--- C++_templates/C++_templates.cpp
+++ C++_templates/C++_templates.cpp
@@ -1,55 +1,41 @@
 
+#include <iostream>													
+
+using namespace std;													
 
 
-		/*
-
-		This program prompts the user to enter
-		three integers and calculates which of
-		the inputted numbers is the greatest and which
-		one is the least using templates.
-
-		*/
-
-
-
-#include <iostream>													// including iostream directive
-
-using namespace std;													// using the std namespace
-
-
-template <class T>													// declaring template with
-T maximum(T num1, T num2, T num3)											// three parameters
+template <class T>													
+T maximum(T num1, T num2, T num3)											
 {
-	return (num1 > num2 && num1 > num3) ? num1 :									// using ternary operator
-		(num2 > num1 && num2 > num3) ? num2 : num3;								// to calculate the greatest than
-}															// the three
+	return (num1 > num2 && num1 > num3) ? num1 :									
+		(num2 > num1 && num2 > num3) ? num2 : num3;								
+}															
 
 
-template <class T>													// declaring template with
-T minimum(T num1, T num2, T num3)											// three parameters
+template <class T>													
+T minimum(T num1, T num2, T num3)											
 {
-	return (num1 < num2 && num1 < num3) ? num1 :									// using ternary pperator
-		(num2 < num1 && num2 < num3) ? num2 : num3;								// to calculate the least than
-}															// the three
+	return (num1 < num2 && num1 < num3) ? num1 :									
+		(num2 < num1 && num2 < num3) ? num2 : num3;								
+}															
 	
 	
-int main()														// the main() function
+int main()														
 {
 
-	int x;														// an integer to hold num1
-	int y;														// an integer to hold num2
-	int z;														// an integer to hold num3
+	int x;														
+	int y;														
+	int z;														
 
-	cout << "\nEnter 3 integers > ";										// prompting the user to enter
-	cin >> x >> y >> z;												// three integers
-
-	cout << "\n\tThe greater than the three is '" <<								// calling the maximum template
+	cout << "\nEnter 3 integers > ";										
+	cin >> x >> y >> z;												
+	cout << "\n\tThe greater than the three is '" <<								
 		maximum(x, y, z) << "'\n";
 
-	cout << "\tThe least than the three is '" <<									// calling the minimum template
+	cout << "\tThe least than the three is '" <<									
 		minimum(x, y, z) << "'\n";
 
 	cout << '\n';
 
-	return 0;													// exiting the program
+	return 0;													
 }
