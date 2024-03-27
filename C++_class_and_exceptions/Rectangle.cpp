92a44d093ff200f5ca0f6fbd8e2eb32f5eca1c64
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 21:52:35 +0200

    Update Rectangle.cpp

--- C++_class_and_exceptions/Rectangle.cpp
+++ C++_class_and_exceptions/Rectangle.cpp
@@ -2,42 +2,42 @@
 
 
 
-#include "Rectangle.h"												// including the Rectangle`s class header
+#include "Rectangle.h"												
 
 
-Rectangle::Rectangle() :lenght(1), width(1)									// assigning 1 to lenght and width
+Rectangle::Rectangle() :lenght(1), width(1)									
 {
 
 }
 
 void Rectangle::set_lenght()
 {
-	cout << "\nEnter the lenght of the rectangle (1.0 - 20.0)> ";						// prompting the user to enter the lenght
+	cout << "\nEnter the lenght of the rectangle (1.0 - 20.0)> ";						
 	cin >> lenght;
 
-	if (lenght < 1 || lenght > 20)										// validating the input
-		throw Exception_lenght(lenght);									// throwing an exception if invalid
+	if (lenght < 1 || lenght > 20)										
+		throw Exception_lenght(lenght);									
 }
 
 void Rectangle::set_width()
 {
-	cout << "\nEnter the width of the rectangle (1.0 - 20.0)> ";						// prompting the user to enter the width
+	cout << "\nEnter the width of the rectangle (1.0 - 20.0)> ";						
 	cin >> width;
 
-	if (width < 1 || width>20)										// validating the input
-		throw Exception_width(width);									// throwing exception if invalid
+	if (width < 1 || width>20)										
+		throw Exception_width(width);									
 }
 
 double Rectangle::get_area()
 {
-	double area = lenght * width;										// calculating the area
-	return area;												// returning the value
+	double area = lenght * width;										
+	return area;												
 }
 
 double Rectangle::get_perimeter()
 {
-	double perimeter = 2 * (lenght + width);								// calculating the perimeter
-	return perimeter;											// returning the value
+	double perimeter = 2 * (lenght + width);								
+	return perimeter;											
 }
 
 Rectangle::~Rectangle()
