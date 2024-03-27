f15036333cf144823b164f6cee69c370aedebff2
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:16:19 +0200

    Update PackagedFood.cpp

--- C++_polimorphism/PackagedFood.cpp
+++ C++_polimorphism/PackagedFood.cpp
@@ -1,18 +1,18 @@
-#include "PackagedFood.h"											// including the PackagedFood header
+#include "PackagedFood.h"											
 
 
-PackagedFood::PackagedFood() : pricePerUnit(0.0)								// declaring the default constructor
+PackagedFood::PackagedFood() : pricePerUnit(0.0)								
 {
 
 }
 
-PackagedFood::PackagedFood(double _pricePerUnit,								// declaring the overloaded constructor to
-	long int _barcode, string _name) : Products(_pricePerUnit, _name)					// assign the parameters to the member variables
+PackagedFood::PackagedFood(double _pricePerUnit,								
+	long int _barcode, string _name) : Products(_pricePerUnit, _name)					
 {
 	pricePerUnit = _pricePerUnit;
 }
 
-void PackagedFood::set_pricePerUnit()										// declaring the setter for the price per unit
+void PackagedFood::set_pricePerUnit()										
 {
 	cout << "\nEnter the price per unit for the product> ";
 	cin >> pricePerUnit;
@@ -24,49 +24,49 @@ void PackagedFood::set_pricePerUnit()										// declaring the setter for the p
 	}
 }
 
-double PackagedFood::get_pricePerUnit()										// declaring the getter for the price per unit
+double PackagedFood::get_pricePerUnit()										
 {
 	return pricePerUnit;
 }
 
-void PackagedFood::set_numUnits()										// declaring the setter for the number of units
+void PackagedFood::set_numUnits()										
 {
 	cout << "\nEnter the number of the units> ";
 	cin >> numUnits;
 
-	if (numUnits <= 0)											// validating the input
+	if (numUnits <= 0)											
 		numUnits = 0;
 }
 
-int PackagedFood::get_numUnits()										// getter for the number of units
+int PackagedFood::get_numUnits()										
 {
 	return numUnits;
 }
 
-double PackagedFood::getTotal()											// declaring the getter for the total price
+double PackagedFood::getTotal()											
 {
 	return double(numUnits) * pricePerUnit;
 }
 
-void PackagedFood::setCode()											// overriding the setter for the barcode
+void PackagedFood::setCode()											
 {
 	cout << "\nEnter the barcode of the product> ";
 	cin >> barcode;
 }
 
-void PackagedFood::setName()											// overriding the setter for the name
+void PackagedFood::setName()											
 {
 	cout << "\nEnter the name of the product> ";
 	cin >> name;
 }
 
-void PackagedFood::scanner()											// declaring the scanner
+void PackagedFood::scanner()											
 {
 	char choice;
 	cout << "\nDo you want to change any of the details (name, code, price) - y/n> ";
 	cin >> choice;
 
-	while (choice != 'y' && choice != 'n')									// validating the input
+	while (choice != 'y' && choice != 'n')									
 	{
 		cout << "\nEnter 'y' for Yes or 'n' for No> ";
 		cin >> choice;
@@ -81,7 +81,7 @@ void PackagedFood::scanner()											// declaring the scanner
 		cout << "\nPrice per unit> ";
 		cin >> pricePerUnit;
 
-		while (pricePerUnit < 0)									// validating the inputted price
+		while (pricePerUnit < 0)									
 		{
 			cout << "\nInvalid input. Try again> ";
 			cin >> pricePerUnit;
@@ -92,7 +92,7 @@ void PackagedFood::scanner()											// declaring the scanner
 
 }
 
-void PackagedFood::printer()											// overriding the virtual printer
+void PackagedFood::printer()											
 {
 	cout << setprecision(2);
 
