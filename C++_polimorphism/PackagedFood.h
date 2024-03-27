969dbadfa76890ddb2de926cad4b3c59b95c55b0
Alex <74405864+AleksandarShishkov@users.noreply.github.com>
2024-03-27 22:17:02 +0200

    Update PackagedFood.h

--- C++_polimorphism/PackagedFood.h
+++ C++_polimorphism/PackagedFood.h
@@ -1,37 +1,36 @@
 #ifndef PACKAGEDFOOD_H
 #define PACKAGEDFOOD_H
-#include "Products.h"												// including the Products header file
+#include "Products.h"												
 
 
 
 
-class PackagedFood :public Products										// defining derived class PackagedFood
+class PackagedFood :public Products										
 {
 
 private:
 
-	double pricePerUnit;											// a private double to hold the price per unit
-	int numUnits;												// a private integer to hold the number of the units
+	double pricePerUnit;											
+	int numUnits;												
 
 public:
 
-	PackagedFood();												// default constructor
-	PackagedFood(double, long int, string);									// constructor with parameters for the price per unit,
-														// the barcode and the name
+	PackagedFood();												
+	PackagedFood(double, long int, string);									
+														
 
-	void set_pricePerUnit();										// setter for the price
-	double get_pricePerUnit();										// getter for the price
+	void set_pricePerUnit();										
+	double get_pricePerUnit();										
 
-	void set_numUnits();											// setter for the units
-	int get_numUnits();											// getter for the units
+	void set_numUnits();											
+	int get_numUnits();											
 
-	double getTotal();											// getter for the total price
+	double getTotal();											
 
-	void setCode() override;										// overriding the virtual setter for the products barcode
-	void setName() override;										// overiding the virtual setter for the products name
-
-	void scanner();												// overiding the pure virtual scanner
-	void printer() override;										// overriding the virtual printer
+	void setCode() override;										
+	void setName() override;										
+	void scanner();												
+	void printer() override;										
 
 
 
